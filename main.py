61eb87e032df05136ebfdd39cdb14a1464e65d4f
DarkSun <igor.maslenikov16@yandex.ru>
2022-11-14 00:13:00 +0300

    Сделал прогу простой вариант

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
-import webbrowser
+from selenium.webdriver.chrome.options import Options
+import time
 
+url = 'https://codeforces.com/problemset/status'
 
-URL = "https://yandex.ru/pogoda/yaroslavl?lat=57.62656&lon=39.893813"
-webbrowser.open(URL)
+#для прописывания прямого пути к бин файлу chrome
+options = Options()
+options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+#открытие браузера
+driver = webdriver.Chrome(chrome_options = options, executable_path=r'chromedriver.exe')
+driver.get(url)
+print("good")
+time.sleep(20)
+
+time_sleep = 10
+stop_prog = 100
+step = 0
+while(step < stop_prog):
+    time.sleep(time_sleep)
+    driver.refresh()
+    print("refresh", step)
+    step = step + 1
+#driver.quit()
